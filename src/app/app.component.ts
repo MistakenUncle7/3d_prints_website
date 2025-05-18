f5f00ee56997fbd1f47f42615e956e01737f3327
MistakenUncle7 <0256318@up.edu.mx>
2025-05-18 17:59:25 -0600

    finished home section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
+import { FeaturesSectionComponent } from "./components/features-section/features-section.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HeroSectionComponent, FeaturesSectionComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
