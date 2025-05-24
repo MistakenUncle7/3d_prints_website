206b18dfe368daea497e151869eae5c21f1ec2bd
MistakenUncle7 <0256318@up.edu.mx>
2025-05-24 14:43:17 -0600

    corrected hero and portfolio section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,13 @@ import { FooterComponent } from "./components/footer/footer.component";
 import { FormComponent } from './components/form/form.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { FaqComponent } from './components/faq/faq.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, FormComponent, PortfolioComponent, AboutUsComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, FormComponent, PortfolioComponent, AboutUsComponent, FaqComponent, ContactUsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,15 @@ import { HomeComponent } from './components/home/home.component';
 import { FormComponent } from './components/form/form.component';
 import { PortfolioComponent } from './components/portfolio/portfolio.component';
 import { AboutUsComponent } from './components/about-us/about-us.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { FaqComponent } from './components/faq/faq.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'form', component: FormComponent},
     {path: 'portfolio', component: PortfolioComponent},
-    {path: 'about_us', component:AboutUsComponent}
+    {path: 'about_us', component:AboutUsComponent},
+    {path: 'contact_us', component:ContactUsComponent},
+    {path: 'faq', component:FaqComponent},
+
 ];
