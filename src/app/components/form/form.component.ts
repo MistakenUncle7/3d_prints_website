1fad22ac13289f8b29fdf89b5724a3497a2dd858
MistakenUncle7 <0256318@up.edu.mx>
2025-05-26 11:55:21 -0600

    updated cotizacion section

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,11 +1,24 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  selectedProfile: 'general' | 'dentist' | null = null;
+  selectedTechnology: 'resina' | 'filamento' | null = null;
 
+  materialOptions: { [key: string]: string[] } = {
+    resina: ['PLA', 'ABS', 'PETG (FDM)'],
+    filamento: ['Estándar', 'Transparente', 'Felxible (Resina)']
+  }
+
+  onProfileChange(profile: 'general' | 'dentist', event: Event) {
+    const checked = (event.target as HTMLInputElement).checked;
+    this.selectedProfile = checked ? profile : null;
+  }
 }
