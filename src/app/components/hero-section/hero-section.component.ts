de7edb13bb8dbf53bc15027d6eadff790b846195
MistakenUncle7 <0256318@up.edu.mx>
2025-05-27 20:01:05 -0600

    finished project

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-hero-section',
@@ -8,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class HeroSectionComponent {
 
+  constructor(private router: Router) {}
+
+  goToForm() {
+    this.router.navigate(['/form']);
+  }
 }
